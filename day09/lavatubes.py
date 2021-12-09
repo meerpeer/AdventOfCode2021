94d7f9d52aad2f93c8ed6a485c3e35b12f3fb680
Merel Van der wal <mevan-de@f0r5s13.codam.nl>
2021-12-09 17:31:49 +0100

    added a few lines

--- day09/lavatubes.py
+++ day09/lavatubes.py
@@ -15,11 +15,13 @@ def check_all_adjecent(lines, i, c):
 
 total_sum = 0
 
-with open("input.txt") as file:
+with open("input_test.txt") as file:
 	lines = file.readlines()
 	for i in range(len(lines)):
 		for c in range(len(lines[i]) - 1):
 			if check_all_adjecent(lines, i, c):
 				total_sum += int(lines[i][c]) + 1
-
-print(total_sum)
+				#lines[i][c] = 'x'
+		print(lines[i])
+print
+print("part one: ", total_sum)
